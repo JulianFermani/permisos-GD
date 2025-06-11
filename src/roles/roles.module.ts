fcb1ff5dcc5da3472552ffcf11072b5e43e799c0
Julián <julianfermani45@gmail.com>
2025-06-10 21:06:22 -0300

    Hotfix modulos

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -5,10 +5,13 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Role } from "./entities/role.entity";
 import { UserEntity } from "src/users/entities/user.entity";
 import { Permission } from "src/permissions/entities/permission.entity";
+import { UsersService } from "src/users/users.service";
+import { JwtService } from "src/jwt/jwt.service";
+import { AuthService } from "src/middlewares/auth.services";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Role, UserEntity, Permission])],
   controllers: [RolesController],
-  providers: [RolesService],
+  providers: [RolesService, AuthService, JwtService, UsersService],
 })
 export class RolesModule {}
